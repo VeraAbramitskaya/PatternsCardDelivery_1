2f78b8865b07f3e89797bdc91a63189e30d50f92
Vera <79658000126@ya.ru>
2023-09-14 23:46:00 +0300

    "Allure"

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -14,6 +14,9 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.delivery.data.DataGenerator.generateDate;
+import io.qameta.allure.selenide.AllureSelenide;
+
+
 
 class DeliveryTest {
 
